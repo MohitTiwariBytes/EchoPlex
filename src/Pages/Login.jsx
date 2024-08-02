d546caa1c118ad443b0c0e6f1ac5ed70f8c2646a
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-08-02 05:46:35 +0000

    animation

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
 import "./Login.css";
 import earth from "../assets/3892168217.gif";
 
 const Login = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      ".login-card",
+      { y: 50, opacity: 0 },
+      { duration: 1, y: 0, opacity: 1, ease: "power2.out" }
+    );
+
+    gsap.fromTo(
+      ".login-form",
+      { x: -50, opacity: 0 },
+      { duration: 1, delay: 0.5, x: 0, opacity: 1, ease: "power2.out" }
+    );
+
+    gsap.fromTo(
+      ".img img",
+      { x: 50, opacity: 0 },
+      { duration: 1, delay: 0.5, x: 0, opacity: 1, ease: "power2.out" }
+    );
+    gsap.fromTo(
+      ".wrapper h1",
+      {
+        y: 1700,
+        opacity: 0,
+      },
+      {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: "power2.out",
+      }
+    );
+  }, []);
+
   return (
     <div className="main-login">
       <div className="login">
@@ -17,7 +51,7 @@ const Login = () => {
           <div className="left">
             <div className="login-form">
               <div className="header">
-                <h1>SignIn</h1>
+                <h1>Sign In</h1>
               </div>
               <input type="text" placeholder="Email" />
               <input type="password" placeholder="Password" />
